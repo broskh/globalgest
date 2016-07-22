7adfca6c05ff56b711f2bfdea34feb2c7d50ffe6
SCHERINET\broskh <SCHERINET\broskh@broskh-fisso-xubuntu.scherinet.local>
2016-07-22 15:12:45 +0200

    Added information about cleaning to photovoltaic systems.

--- servizi/impianti-fotovoltaici/index.php
+++ servizi/impianti-fotovoltaici/index.php
@@ -32,11 +32,11 @@ HTML;
 // 		fine variabili di testo-->
  	
 //  	<--inizio della creazione della struttura del contenuto specifico della pagina
- 	$about_us = $body->ownerDocument->createElement ("section");
- 	$about_us->setAttribute ("id", "impianti-fotovoltaici");
- 	$about_us->setAttribute ("class", "section_content");
+ 	$FTV_system = $body->ownerDocument->createElement ("section");
+ 	$FTV_system->setAttribute ("id", "impianti-fotovoltaici");
+ 	$FTV_system->setAttribute ("class", "section_content");
  	
- 	$container = $about_us->ownerDocument->createElement ("div");
+ 	$container = $FTV_system->ownerDocument->createElement ("div");
  	$container->setAttribute ("class", "container");
  	
 // 		aggiungo titolo
@@ -75,8 +75,170 @@ HTML;
 	$main_feature->appendChild ($col);
  	$row1->appendChild ($main_feature);
  	$container->appendChild ($row1);
- 	$about_us->appendChild ($container);
- 	$body->appendChild($about_us);
+ 	$FTV_system->appendChild ($container);
+ 	
+ 	//  	<<--LAVAGGIO PANNELLI
+ 	$par5_text = <<<HTML
+ 	Avete un impianto fotovoltaico? 
+HTML;
+ 	$par6_text = <<<HTML
+ 	Vi siete mai chiesti se i vostri pannelli solari, quando non lavati regolarmente e 
+ 			correttamente, rendono meno? 
+HTML;
+ 	$par7_text = <<<HTML
+ 	La risposta &egrave; s&igrave;! Questo significa una perdita di denaro importante.
+HTML;
+ 	$par8_text = <<<HTML
+ 	Acqua-Osmotizzata: La Nostra soluzione ai tuoi problemi di pulizia.
+HTML;
+ 	$par9_text = <<<HTML
+ 	&Egrave; questo il segreto per ottenere il massimo risultato dal lavaggio dei tuoi pannelli 
+ 			fotovoltaici e migliorare così il rendimento del tuo impianto.
+HTML;
+ 	$par10_text = <<<HTML
+ 	L'acqua-osmotizzata ha la caratteristica, a differenza dell'acqua comunemente utilizzata per 
+ 			la pulizia delle superfici, di essere completamente priva di impurit&agrave;. Gli ioni caricati 
+ 			attraggono e dissolvono le particelle ed i residui di sporco, lasciando la superficie 
+ 			dei tuoi pannelli perfettamente pulita e priva di aloni.
+HTML;
+ 	$par11_text = <<<HTML
+ 	Per questo motivo noi della Global Gest s.r.l. utilizziamo l'innovativa macchina di lavaggio per 
+ 			pannelli fotovoltaici esclusivamente con acqua osmotizzata. La macchina da noi usata &egrave; 
+ 			dotata di un sistema a 4 stadi di filtraggio:
+HTML;
+ 	$list1_text = <<<HTML
+ 	Filtro di sedimentazione: rimuove le sostanze con diametro superiore ai 5μm.
+HTML;
+ 	$list2_text = <<<HTML
+ 	Filtro a carboni attivi: rimuove il cloro.
+HTML;
+ 	$list3_text = <<<HTML
+ 	Membrana ad osmosi inversa: elimina fino al 98% delle sostanze disciolte nell'acqua.
+HTML;
+ 	$list4_text = <<<HTML
+ 	Cartuccia a resine deionizzanti: elimina le restanti sostanze.
+HTML;
+ 	$par12_text = <<<HTML
+ 	L'acqua in questo modo &egrave; pura al 100% (0 ppm) ed agisce come uno straordinario solvente che attrae e 
+ 			dissolve lo sporco sulle superfici, grazie alla propria natura dipolare (capacità di reagire 
+ 			sia con cariche positive che con cariche negative). 
+HTML;
+ 	$par13_text = <<<HTML
+ 	Possiamo affermare che la perdita minima di resa di un impianto fotovoltaico con scarsa manutenzione, ad un anno 
+ 			dalla sua posa, si può aggirare dal 10% al 30% della sua produzione di picco.
+HTML;
+ 	$par14_text = <<<HTML
+ 	La spesa per questa operazione &egrave; in realt&agrave; un guadagno. Per ottenere dal vostro impianto il miglior 
+ 			rendimento, e per valorizzare il vostro investimento, il lavaggio programmato dei pannelli &egrave; il 
+ 			sistema più efficacie. 
+HTML;
+
+ 	addBR($FTV_system);
+ 	addBR($FTV_system);
+ 	$cleaning = $FTV_system->ownerDocument->createElement ("div");
+ 	$cleaning->setAttribute ("class", "container");
+ 	
+ 	// 		aggiungo titolo
+ 	addContentTitle($cleaning, ["Il nostro ", "SISTEMA DI PULIZIA"], "");
+ 	
+ 	//  	aggiungo il contenuto
+ 	$row2 = $cleaning->ownerDocument->createElement ("div");
+ 	$row2->setAttribute ("class", "row");
+ 	
+ 	$main_feature2 = $row2->ownerDocument->createElement ("div");
+ 	$main_feature2->setAttribute ("class", "main_feature");
+ 	
+ 	$col2 = $main_feature2->ownerDocument->createElement ("div");
+ 	$col2->setAttribute ("class", "col-md-12 col-xs-12 col-sm-12");
+ 	
+ 	$col2->appendChild ($col2->ownerDocument->createElement ("p", $par5_text));
+ 	$col2->appendChild ($col2->ownerDocument->createElement ("p", $par6_text));
+ 	$col2->appendChild ($col2->ownerDocument->createElement ("p", $par7_text));
+ 	
+ 	$main_feature2->appendChild ($col2);
+ 	addBR($main_feature2);
+ 	
+ 	$col_section1 = $main_feature2->ownerDocument->createElement ("div");
+ 	$col_section1->setAttribute ("class", "col-md-12 col-xs-12 col-sm-12 father-vertical-align");
+ 	
+ 	$col3 = $col_section1->ownerDocument->createElement ("div");
+ 	$col3->setAttribute ("class", "col-md-3 col-xs-12 col-sm-3 child-vertical-align");
+ 	
+ 	$image1 = $col3->ownerDocument->createElement ("img");
+ 	$image1->setAttribute ("src", dir_img . "/FTV_cleaning/lavapannelli1.jpg");
+ 	$image1->setAttribute ("class", "img-responsive img-center img-limit");
+ 	
+ 	$col3->appendChild ($image1);
+ 	$col_section1->appendChild ($col3);
+ 	
+ 	$col4 = $col_section1->ownerDocument->createElement ("div");
+ 	$col4->setAttribute ("class", "col-md-9 col-xs-12 col-sm-9 child-vertical-align");
+
+ 	$col4->appendChild ($col4->ownerDocument->createElement ("p", $par8_text));
+ 	$col4->appendChild ($col4->ownerDocument->createElement ("p", $par9_text));
+ 	$col4->appendChild ($col4->ownerDocument->createElement ("p", $par10_text));
+ 	
+ 	$col_section1->appendChild ($col4);
+ 	$main_feature2->appendChild ($col_section1);
+ 	
+ 	$col_section2 = $main_feature2->ownerDocument->createElement ("div");
+ 	$col_section2->setAttribute ("class", "col-md-12 col-xs-12 col-sm-12 father-vertical-align");
+ 	
+ 	$col5 = $col_section2->ownerDocument->createElement ("div");
+ 	$col5->setAttribute ("class", "col-md-3 col-md-push-9 col-xs-12 col-sm-3 col-sm-push-9 child-vertical-align");
+ 	
+ 	$image2 = $col5->ownerDocument->createElement ("img");
+ 	$image2->setAttribute ("src", dir_img . "/FTV_cleaning/lavapannelli2.jpg");
+ 	$image2->setAttribute ("class", "img-responsive img-center img-limit");
+ 	
+ 	$col5->appendChild ($image2);
+ 	$col_section2->appendChild ($col5);
+ 	
+ 	$col6 = $col_section2->ownerDocument->createElement ("div");
+ 	$col6->setAttribute ("class", "col-md-9 col-md-pull-3 col-xs-12 col-sm-9 col-sm-pull-3 child-vertical-align");
+
+ 	$col6->appendChild ($col6->ownerDocument->createElement ("p", $par11_text));
+ 	$list = $col6->ownerDocument->createElement ("ul");
+ 	$list->setAttribute ("class", "cleaning-machine-list");
+ 	$list->appendChild ($list->ownerDocument->createElement ("li", $list1_text));
+ 	$list->appendChild ($list->ownerDocument->createElement ("li", $list2_text));
+ 	$list->appendChild ($list->ownerDocument->createElement ("li", $list3_text));
+ 	$list->appendChild ($list->ownerDocument->createElement ("li", $list4_text));
+ 	$col6->appendChild ($list); 	
+ 	
+ 	$col_section2->appendChild ($col6);
+ 	$main_feature2->appendChild ($col_section2);
+ 	addBR($main_feature2);
+ 	
+ 	$col_section3 = $main_feature2->ownerDocument->createElement ("div");
+ 	$col_section3->setAttribute ("class", "col-md-12 col-xs-12 col-sm-12 father-vertical-align");
+ 	
+ 	$col7 = $col_section3->ownerDocument->createElement ("div");
+ 	$col7->setAttribute ("class", "col-md-3 col-xs-12 col-sm-3 child-vertical-align");
+ 	
+ 	$image3 = $col7->ownerDocument->createElement ("img");
+ 	$image3->setAttribute ("src", dir_img . "/FTV_cleaning/lavapannelli3.jpg");
+ 	$image3->setAttribute ("class", "img-responsive img-center img-limit");
+ 	
+ 	$col7->appendChild ($image3);
+ 	$col_section3->appendChild ($col7);
+ 	
+ 	$col8 = $col_section3->ownerDocument->createElement ("div");
+ 	$col8->setAttribute ("class", "col-md-9 col-xs-12 col-sm-9 child-vertical-align");
+ 	
+ 	$col8->appendChild ($col8->ownerDocument->createElement ("p", $par12_text));
+ 	$col8->appendChild ($col8->ownerDocument->createElement ("p", $par13_text));
+ 	$col8->appendChild ($col8->ownerDocument->createElement ("p", $par14_text));
+ 	
+ 	$col_section3->appendChild ($col8);
+ 	$main_feature2->appendChild ($col_section3);
+ 	
+ 	$row2->appendChild ($main_feature2);
+ 	$cleaning->appendChild ($row2);
+ 	$FTV_system->appendChild($cleaning);
+ 	// 	FINE LAVAGGIO-->>
+ 	
+ 	$body->appendChild($FTV_system);
 //  	fine della struttura-->
 	
 //  	<------------------------------------------------------>
